9dd4a29b1b6d4ddaf93d6c82e0b1d7b66a5bd7c0
CamrynBowWow <bowdencamryn@gmail.com>
2025-01-29 19:09:52 +0200

    UseEffect Hook Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const Card = ({ title }) => {
+	const [count, setCount] = useState(0);
 	const [hasLiked, setHasLiked] = useState(false);
 
+	useEffect(() => {
+		console.log(`${title} has been liked: ${hasLiked}`);
+	}, [hasLiked]);
+
 	return (
-		<div className='card'>
-			<h2>{title}</h2>
+		<div className='card' onClick={() => setCount(count + 1)}>
+			{/* Would be done for more professional code */}
+			{/* <div className='card' onClick={() => setCount((prevState) => prevState + 1)}> */}
+			<h2>
+				{title} <br /> {count || null}
+			</h2>
 
 			<button onClick={() => setHasLiked(!hasLiked)}>{hasLiked ? 'Liked' : 'Like'}</button>
 		</div>
