7d08a33467cf6dedfecfdfac30bfb6616672e506
CamrynBowWow <bowdencamryn@gmail.com>
2025-01-29 14:36:10 +0200

    Props Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css';
 
+const Card = ({ title }) => {
+	return (
+		<div>
+			<h2>{title}</h2>
+		</div>
+	);
+};
+
 const App = () => {
-	return <h2>Functional Arrow Component </h2>;
+	return (
+		<div>
+			<h2>Functional Arrow Component </h2>;
+			<Card title='Star Wars' />
+			<Card title='Avatar' />
+		</div>
+	);
 };
 
 export default App;
