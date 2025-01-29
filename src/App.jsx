47cde51d484f6dddf029336f186f49e7c0307908
CamrynBowWow <bowdencamryn@gmail.com>
2025-01-29 19:10:58 +0200

    React Snippets Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,7 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
-const Card = ({ title }) => {
-	const [count, setCount] = useState(0);
-	const [hasLiked, setHasLiked] = useState(false);
-
-	useEffect(() => {
-		console.log(`${title} has been liked: ${hasLiked}`);
-	}, [hasLiked]);
-
-	return (
-		<div className='card' onClick={() => setCount(count + 1)}>
-			{/* Would be done for more professional code */}
-			{/* <div className='card' onClick={() => setCount((prevState) => prevState + 1)}> */}
-			<h2>
-				{title} <br /> {count || null}
-			</h2>
-
-			<button onClick={() => setHasLiked(!hasLiked)}>{hasLiked ? 'Liked' : 'Like'}</button>
-		</div>
-	);
-};
+import React from 'react';
 
 const App = () => {
-	return (
-		<div className='card-container'>
-			<h2>Functional Arrow Component </h2>;
-			<Card title='Star Wars' />
-			<Card title='Avatar' />
-		</div>
-	);
+	return <div>App</div>;
 };
 
 export default App;
